0528c339c6a74100652761dd58ebebbf5dfc1f5b
TurBoss <j.l.toledano.l@gmail.com>
2017-05-13 21:54:56 +0200

    try to catch exception

--- hazzy/modules/camview/stream.py
+++ hazzy/modules/camview/stream.py
@@ -5,46 +5,51 @@ import re
 import time
 import cgi
 
+import errno
+import socket
+
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 
 class HttpServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        try:
-            self.path = re.sub('[^.a-zA-Z0-9]', "", str(self.path))
-            if self.path == "" or self.path is None or self.path[:1] == ".":
-                return
+        self.path = re.sub('[^.a-zA-Z0-9]', "", str(self.path))
+        if self.path == "" or self.path is None or self.path[:1] == ".":
+            return
 
-            if self.path.endswith("index.html"):
-                file_path = os.path.join(os.curdir, self.path)
-                with open(file_path) as f:
-                    self.send_response(200)
-                    self.send_header('Content-type', 'text/html')
-                    self.end_headers()
-                    self.wfile.write(f.read())
-                return
-
-            if self.path.endswith("stream.mjpeg"):
+        if self.path.endswith("index.html"):
+            file_path = os.path.join(os.curdir, self.path)
+            with open(file_path) as f:
                 self.send_response(200)
-                self.wfile.write("Content-Type: multipart/x-mixed-replace; boundary=--aaboundary")
-                self.wfile.write("\r\n\r\n")
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(f.read())
+            return
 
-                running = True
-                while running:
+        if self.path.endswith("stream.mjpeg"):
+            print("init")
 
-                        img = self.server.handler()
-                        self.wfile.write("--aaboundary\r\n")
-                        self.wfile.write("Content-Type: image/jpeg\r\n")
-                        self.wfile.write("Content-length: {0}\r\n\r\n".format(len(img)))
-                        self.wfile.write(img)
-                        self.wfile.write("\r\n\r\n\r\n")
-                        time.sleep(0.1)
+            self.send_response(200)
+            self.wfile.write("Content-Type: multipart/x-mixed-replace; boundary=--aaboundary")
+            self.wfile.write("\r\n\r\n")
 
-                return
+            running = True
+            while running:
+                print("running")
+                try:
+                    img = self.server.handler()
+                    self.wfile.write("--aaboundary\r\n")
+                    self.wfile.write("Content-Type: image/jpeg\r\n")
+                    self.wfile.write("Content-length: {0}\r\n\r\n".format(len(img)))
+                    self.wfile.write(img)
+                    self.wfile.write("\r\n\r\n\r\n")
+                    time.sleep(0.1)
 
-        except IOError:
-            self.send_error(404, 'File Not Found: %s' % self.path)
+                except Exception as e:
+                    print("Error: {0}".format(e))
+                    running = False
+            return
 
     def do_POST(self):
         try:
@@ -67,12 +72,12 @@ class HttpServerHandler(BaseHTTPRequestHandler):
             print(e)
 
 
-class MyHTTPServer(HTTPServer):
+class StreamHTTPServer(HTTPServer):
     """this class is necessary to allow passing custom request handler into
        the RequestHandlerClass"""
 
-    def __init__(self, server_address, RequestHandlerClass, handler):
-        HTTPServer.__init__(self, server_address, RequestHandlerClass)
+    def __init__(self, server_address, request_handler_class, handler):
+        HTTPServer.__init__(self, server_address, request_handler_class)
         self.handler = handler
 
 
@@ -85,8 +90,8 @@ class HttpServer:
         self.server = None
 
     def run(self):
-        self.server = MyHTTPServer((self.host, self.port), HttpServerHandler,
-                                   self.handler)
+        self.server = StreamHTTPServer((self.host, self.port), HttpServerHandler,
+                                       self.handler)
         self.server.serve_forever()
 
     def stop(self):
