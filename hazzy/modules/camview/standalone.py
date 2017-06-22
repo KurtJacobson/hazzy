9f238d483bff2e4e0bddd65cfc950fcec50008d2
TurBoss <j.l.toledano.l@gmail.com>
2017-06-22 23:46:37 +0200

    add license header

--- hazzy/modules/camview/standalone.py
+++ hazzy/modules/camview/standalone.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#   Copyright (c) 2017 Kurt Jacobson
+#     <kurtjacobson@bellsouth.net>
+#
+#   This file is part of Hazzy.
+#
+#   Hazzy is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Hazzy is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Hazzy.  If not, see <http://www.gnu.org/licenses/>.
+
 import threading
 
 from video import VideoDev
@@ -15,11 +36,7 @@ class ControlThread(threading.Thread):
         self.args = args
 
     def run(self):
-        print("Starting {0}\n".format(self.name))
-
         self.callback.run()
-
-        print("Exiting {0}\n".format(self.name))
 
 
 def main():
--- hazzy/modules/camview/video.py
+++ hazzy/modules/camview/video.py
@@ -1,5 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+#   Copyright (c) 2017 Kurt Jacobson
+#     <kurtjacobson@bellsouth.net>
+#
+#   This file is part of Hazzy.
+#
+#   Hazzy is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Hazzy is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Hazzy.  If not, see <http://www.gnu.org/licenses/>.
+
 import cv2
-import subprocess
 
 # check if opencv3 is used, so we will have to change attribut naming
 from pkg_resources import parse_version
@@ -24,27 +43,6 @@ class VideoDev:
 
         self.jpeg_frame = cv2.imencode('.jpg', self.frame)[1].tostring()
 
-        # 0  = CAP_PROP_POS_MSEC        Current position of the video file in milliseconds.
-        # 1  = CAP_PROP_POS_FRAMES      0-based index of the frame to be decoded/captured next.
-        # 2  = CAP_PROP_POS_AVI_RATIO   Relative position of the video file
-        # 3  = CAP_PROP_FRAME_WIDTH     Width of the frames in the video stream.
-        # 4  = CAP_PROP_FRAME_HEIGHT    Height of the frames in the video stream.
-        # 5  = CAP_PROP_FPS             Frame rate.
-        # 6  = CAP_PROP_FOURCC          4-character code of codec.
-        # 7  = CAP_PROP_FRAME_COUNT     Number of frames in the video file.
-        # 8  = CAP_PROP_FORMAT          Format of the Mat objects returned by retrieve() .
-        # 9  = CAP_PROP_MODE            Backend-specific value indicating the current capture mode.
-        # 10 = CAP_PROP_BRIGHTNESS      Brightness of the image (only for cameras).
-        # 11 = CAP_PROP_CONTRAST        Contrast of the image (only for cameras).
-        # 12 = CAP_PROP_SATURATION      Saturation of the image (only for cameras).
-        # 13 = CAP_PROP_HUE             Hue of the image (only for cameras).
-        # 14 = CAP_PROP_GAIN            Gain of the image (only for cameras).
-        # 15 = CAP_PROP_EXPOSURE        Exposure (only for cameras).
-        # 16 = CAP_PROP_CONVERT_RGB     Boolean flags indicating whether images should be converted to RGB.
-        # 17 = CAP_PROP_WHITE_BALANCE   Currently unsupported
-        # 18 = CAP_PROP_RECTIFICATION   Rectification flag for stereo cameras
-        #                               (note: only supported by DC1394 v 2.x backend currently)
-
         if OPCV3:
             self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
             self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
@@ -52,6 +50,7 @@ class VideoDev:
         else:
             self.cam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, self.frame_width)
             self.cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, self.frame_height)
+            self.cam.set(cv2.cv.CV_CAP_PROP_FPS, 30)
 
     def run(self):
         running = True
