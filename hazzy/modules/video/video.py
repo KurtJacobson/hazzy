976e218b38ec2ebfe7f0a0052d1ab7320b1b9c1a
TurBoss <j.l.toledano.l@gmail.com>
2017-09-07 15:30:14 +0200

    Version

--- hazzy/modules/video/video.py
+++ hazzy/modules/video/video.py
@@ -4,6 +4,7 @@ import gi
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
+gi.require_version('GdkX11', '3.0')
 gi.require_version('GstVideo', '1.0')
 
 from gi.repository import GObject, Gst, Gtk
