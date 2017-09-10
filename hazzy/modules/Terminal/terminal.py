72f08d3201aeeb73c1437383514a2ce591a61f45
Kurt Jacobson <kcjengr@gmail.com>
2017-09-10 12:21:02 -0400

    reset terminal on "ctrl + d" instead of exit

--- hazzy/modules/Terminal/terminal.py
+++ hazzy/modules/Terminal/terminal.py
@@ -8,8 +8,8 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
 gi.require_version('Vte', '2.91')
 
-from gi.repository import Gtk, Vte
-from gi.repository import GLib
+from gi.repository import Gtk, Gdk
+from gi.repository import Vte, GLib
 
 PYDIR = os.path.join(os.path.dirname(__file__))
 
@@ -33,11 +33,20 @@ class Terminal(Gtk.ScrolledWindow):
 
         self.terminal.set_font_scale(1.1)
 
+        self.terminal.connect('key-press-event', self.on_key_press)
+
         self.set_size_request(200, 200)
         self.set_hexpand(True)
         self.set_vexpand(True)
 
         self.add(self.terminal)
+
+    def on_key_press(self, widget, event):
+        kv = event.keyval
+        if event.get_state() & Gdk.ModifierType.CONTROL_MASK:
+            if kv == Gdk.KEY_d:
+                self.terminal.feed_child("reset\n", -1)
+                return True
 
 def main():
     win = Gtk.Window()
