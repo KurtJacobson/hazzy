6435ae14b0508da17e7b81f9abf950460c65817e
Kurt Jacobson <kcjengr@gmail.com>
2017-09-12 19:40:32 -0400

    some work on DRO widget

---
+++ hazzy/modules/DroWidget/dro.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import gi
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+gi.require_version('Vte', '2.91')
+
+from gi.repository import Gtk, Gdk
+
+PYDIR = os.path.join(os.path.dirname(__file__))
+
+class Dro(Gtk.Grid):
+
+    def __init__(self):
+        Gtk.Grid.__init__(self)
+
+        axes = os.environ.get('TRAJ_COORDINATES', None)
+        print axes
+
+
+
