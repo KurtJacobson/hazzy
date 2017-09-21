40e7903d447b03effe014711acf1a7db8ccbe3d0
Kurt Jacobson <kcjengr@gmail.com>
2017-09-21 17:18:23 -0400

    no need for commands class

--- hazzy/utilities/commands.py
+++ hazzy/utilities/commands.py
@@ -6,71 +6,69 @@ import linuxcnc
 from utilities import logger
 log = logger.get(__name__)
 
-class Commands():
-    def __init__(self):
-        self.stat = linuxcnc.stat()
-        self.command = linuxcnc.command()
+stat = linuxcnc.stat()
+command = linuxcnc.command()
 
-    def estop(self):
-        self.set_state(linuxcnc.STATE_ESTOP)
+def estop():
+    set_state(linuxcnc.STATE_ESTOP)
 
-    def estop_reset(self):
-        self.set_state(linuxcnc.STATE_ESTOP_RESET)
+def estop_reset():
+    set_state(linuxcnc.STATE_ESTOP_RESET)
 
-    def machine_off(self):
-        self.set_state(linuxcnc.STATE_OFF)
+def machine_off():
+    set_state(linuxcnc.STATE_OFF)
 
-    def machine_on(self):
-        self.set_state(linuxcnc.STATE_ON)
+def machine_on():
+    set_state(linuxcnc.STATE_ON)
 
-    def mist_on(self):
-        self.command.mist(1)
+def mist_on():
+    command.mist(1)
 
-    def mist_off(self):
-        self.command.mist(0)
+def mist_off():
+    command.mist(0)
 
-    def flood_on(self):
-        self.command.flood(1)
+def flood_on():
+    command.flood(1)
 
-    def flood_off(self):
-        self.command.flood(0)
+def flood_off():
+    command.flood(0)
 
-    def set_mode(self, mode):
-        self.stat.poll()
-        if self.stat.task_mode == mode:
-            return
-        self.command.mode(mode)
-        self.command.wait_complete()
+def set_mode(mode):
+    stat.poll()
+    if stat.task_mode == mode:
+        return
+    command.mode(mode)
+    command.wait_complete()
 
-    def set_state(self, state):
-        self.stat.poll()
-        if self.stat.state == state:
-            return
-        self.command.state(state)
-        self.command.wait_complete()
+def set_state(state):
+    stat.poll()
+    if stat.state == state:
+        return
+    command.state(state)
+    command.wait_complete()
 
-    def set_motion_mode(self, mode):
-        self.stat.poll()
-        if self.stat.motion_mode == mode:
-            return
-        self.command.teleop_enable(0)
-        self.command.traj_mode(mode)
-        self.command.wait_complete()
+def set_motion_mode(mode):
+    stat.poll()
+    if stat.motion_mode == mode:
+        return
+    command.teleop_enable(0)
+    command.traj_mode(mode)
+    command.wait_complete()
 
-    def issue_mdi(self, mdi_command):
-        if self.set_mode(linuxcnc.MODE_MDI):
-            log.info("Issuing MDI command: {}".format(mdi_command))
-            self.command.mdi(mdi_command)
+def issue_mdi(mdi_command):
+    if set_mode(linuxcnc.MODE_MDI):
+        log.info("Issuing MDI command: {}".format(mdi_command))
+        command.mdi(mdi_command)
 
-    def set_work_offset(self, axis, value):
-        offset_command = 'G10 L20 P%d %s%.12f' % (self.stat.g5x_index, axis, value)
-        self.issue_mdi(offset_command)
-        self.set_mode(linuxcnc.MODE_MANUAL)
+def set_work_offset(axis, value):
+    offset_command = 'G10 L20 P%d %s%.12f' % (stat.g5x_index, axis, value)
+    issue_mdi(offset_command)
+    set_mode(linuxcnc.MODE_MANUAL)
 
-    def is_moving(self):
-        '''Check if machine is moving due to MDI, program execution, etc.'''
-        if self.stat.state == linuxcnc.RCS_EXEC:
-            return True
-        else:
-            return self.stat.task_mode == linuxcnc.MODE_AUTO \
-                and self.stat.interp_state != linuxcnc.INTERP_IDLE
+def is_moving():
+    '''Check if machine is moving due to MDI, program execution, etc.'''
+    if stat.state == linuxcnc.RCS_EXEC:
+        return True
+    else:
+        return stat.task_mode == linuxcnc.MODE_AUTO \
+            and stat.interp_state != linuxcnc.INTERP_IDLE
