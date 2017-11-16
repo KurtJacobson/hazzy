d63395497a11bd499f1b7b717acc31273341bcd9
Kurt Jacobson <kcjengr@gmail.com>
2017-11-16 13:16:33 -0500

    Fix: g_value_get_int: assertion 'G_VALUE_HOLDS_INT (value)' failed

--- hazzy/widget_factory/entry_widgets.py
+++ hazzy/widget_factory/entry_widgets.py
@@ -39,10 +39,43 @@ from utilities import preferences as prefs
 from widget_factory.TouchPads import keyboard
 
 
-class TextEntry(Gtk.Entry):
-    def __init__(self):
-        Gtk.Entry.__init__(self)
 
+class ValidatableEntry(Gtk.Entry, Gtk.Editable):
+    __gtype_name__ = 'ValidatableEntry'
+    __gsignals__ = {
+        'validate-text': (GObject.SignalFlags.RUN_FIRST, None, (str, int, int)),
+    }
+
+    # Many of the entries used in Hazzy listen to the `insert-text` signal and
+    # then validate the entry before actually setting the text.  The problem is
+    # that PyGI signals do not properly handle return values from methods called
+    # by a signal, resulting in incorrect cursor positions and the message:
+    # `Warning: g_value_get_int: assertion 'G_VALUE_HOLDS_INT (value)' failed Gtk.main()`
+
+    # The solution is to inherent Gtk.Editable and Gtk.Entry and override the
+    # default `do_insert_text` method so that is returns the popper pointer position.
+
+    # References:
+    #   https://stackoverflow.com/questions/38815694/gtk-3-position-attribute-on-insert-text-signal-from-gtk-entry-is-always-0
+    #   https://stackoverflow.com/questions/40074977/how-to-format-the-entries-in-gtk-entry/40163816
+
+    def __init__(self):
+        super(ValidatableEntry, self).__init__()
+
+    # This can be overridden to perform validation, or in cases were the cursor
+    # pos is not effected by the validation the, `validate-text` signal can be
+    # used to call a method to perform the validation without having to subclass.
+    def do_insert_text(self, new_text, length, position):
+        self.get_buffer().insert_text(position, new_text, length)
+        self.emit('validate-text', new_text, length, position)
+        return position + length
+
+
+class TextEntry(ValidatableEntry):
+    def __init__(self):
+        super(ValidatableEntry, self).__init__()
+
+        self.use_vertual_keyboard = True
         self.activate_on_focus_out = False
         self._was_activated = False
 
@@ -55,13 +88,13 @@ class TextEntry(Gtk.Entry):
 
     def on_focus_in(self, widegt, event):
         self.previous_value = self.get_text()
-        keyboard.show(self)
+        if self.use_vertual_keyboard:
+            keyboard.show(self)
 
     def on_focus_out(self, widegt, event):
         if self._was_activated:
             return
         if self.activate_on_focus_out:
-            # FixMe This does not work :(
             self.do_activate(widegt)
         else:
             # Revert
@@ -77,3 +110,9 @@ class TextEntry(Gtk.Entry):
         if event.keyval == Gdk.KEY_Escape:
             self.set_text(self.previous_value) # Revert
             self.get_toplevel().set_focus(None)
+
+    def set_activate_on_focus_out(self, setting):
+        self.activate_on_focus_out = setting
+
+    def set_use_vertual_keyboard(self, setting):
+        self.use_vertual_keyboard = setting
