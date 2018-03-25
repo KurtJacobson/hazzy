3a0300b2891c0d45c9185f08594bbfd9aac5da6c
Kurt Jacobson <kcjengr@gmail.com>
2018-03-25 14:04:43 -0400

    Test sending MDI using WS

--- hazzy/utilities/WebSocket/server.py
+++ hazzy/utilities/WebSocket/server.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+
+import json
+import linuxcnc
 from websocket_server import WebsocketServer
+
+command = linuxcnc.command()
 
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
-    print("New client connected and was given id %d" % client['id'])
+    client_id = client['id']
+    client_address = client['address']
+    print("New client connected and was given id {}".format(client_id))
+    server.send_message(client, "This client's ID is {0} and has address {1}".format(client_id, client_address))
     server.send_message_to_all("Hey all, a new client has joined us")
 
 
@@ -14,9 +23,23 @@ def client_left(client, server):
 
 # Called when a client sends a message
 def message_received(client, server, message):
-    if len(message) > 200:
-        message = message[:200] + '..'
-    print("Client(%d) said: %s" % (client['id'], message))
+    parsed_msg = json.loads(message)
+    print json.dumps(parsed_msg, indent=4, sort_keys=True)
+
+    if parsed_msg.get('type') == 'command':
+        process_command(client, parsed_msg)
+
+
+def process_command(client, data):
+    command_type = data.get('command_type')
+    if hasattr(command, command_type):
+        try:
+            cmd = getattr(command, command_type)
+            text = data.get('command_text')
+            command.mode(linuxcnc.MODE_MDI)
+            cmd(text)
+        except Exception as e:
+            server.send_message(client, "ERROR: {}".format(e))
 
 
 PORT = 9001
